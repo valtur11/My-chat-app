1b145664b69c5e4ee7b32c9845150b3a61da7dee
Valentin Ratchev <valentinratchev@gmail.com>
2020-05-09 19:52:15 +0300

    add user signup service

--- api/Authentication/controller.js
+++ api/Authentication/controller.js
@@ -1,13 +1,28 @@
 const createAuthMethods = (lib) => {
-  return {
-    /**
-     * Creates new token that returns the payload argument and the token
+  /**
+     * Helper function that creates new token that returns the payload argument and the token. should be private and immutable.
      * @param {Object} payload user details
      */
-    createToken(payload) {
-      const options = { expiresIn: '2h' };
-      const token = lib.jwt.sign(payload, lib.env.JWT_SECRET, [options]);
-      return { ...payload, token };
+  function createToken (payload) {
+    const options = { expiresIn: '2h' };
+    const token = lib.jwt.sign(payload, lib.env.JWT_SECRET, [options]);
+    return { ...payload, token };
+  }
+
+  return {
+    createToken,
+    async signup(userDetails) {
+      try {
+        const User = await lib.user.create(userDetails);
+        const UserAndToken = createToken(User);
+        return { data: {...UserAndToken}, status: 201 };
+      } catch (error) {
+        if(error.code === 11000) {
+          return { status: 400 };
+        } else {
+          return { status: 500 };
+        }
+      }
     }
   };
 };
--- api/test/unit/authentication.spec.js
+++ api/test/unit/authentication.spec.js
@@ -1,18 +1,26 @@
+/**
+ * Authentication feature's spec file
+ * @todo sign in component shows when only the user is not authorized
+ * @todo password length limit
+ * @todo rate limiter
+  * @todo isMatch is always true check test
+ */
 const expect = require('chai').expect;
-const createAuthMethods = require('../../Authentication/controller');
+const createAuthMethods = require('../../authentication/controller');
 const sinon = require('sinon');
 const expectedUserDetails = { email: 'tester@test.com'};
 const token = 'header.payload.signature';
+const sign = sinon.fake.returns(token);
+const JWT_SECRET = 'This is a JWT secret for mocha testing';
+const options = {expiresIn: '2h'};
+const commonAuthOptions = {
+  jwt: { sign },
+  env: { JWT_SECRET }
+};
 
 describe('Authentication', function() {
   describe('Create jwt token', function() {
-    const sign = sinon.fake.returns(token);
-    const JWT_SECRET = 'This is a JWT secret for mocha testing';
-    const options = {expiresIn: '2h'};
-    const { createToken } = createAuthMethods({
-      jwt: { sign },
-      env: { JWT_SECRET }
-    });
+    const { createToken } = createAuthMethods({ ...commonAuthOptions });
     let userDetailsAndToken;
 
     beforeEach(function () {
@@ -29,23 +37,70 @@ describe('Authentication', function() {
     });
 
     it('should return the user details and token', function () {
-      // check args
       expect(userDetailsAndToken).to.be.an('object').that.include({ ...expectedUserDetails, token });
     });
   });
-  // sign in component shows when only the user is not authorized
-  //password length
-  //rate limiter
-  //isMatch is always true check test
-  describe('Sign up', function () {
-    it('should return status 200 with user details and token', /*function () {
-      // const expectStatus = 200;
-      // singup service
-    }*/);
 
-    it('should return 400 when email or username is taken');
+  describe('Sign up', function () {
+    it('should return status 201 with user details and token', async function () {
+      const { signup } = createAuthMethods({
+        ...commonAuthOptions,
+        user: { create: sinon.fake.returns(expectedUserDetails)}
+      });
+      // singup service, independent from express
+      // res.status(obj.status).json(object.userDetails);
+      const status = 201;
+      const newUser = await signup(expectedUserDetails);
+      expect(newUser).to.be.an('object').that.include({ status });
+      expect(newUser.data).to.be.an('object').that.include({ ...expectedUserDetails, token });
+    });
+
+    it('should return 400 when email or username is taken', async function () {
+      const takenError = new Error('Sorry, but this email or username is taken.');
+      takenError.code =  11000;
+      const { signup } = createAuthMethods({
+        ...commonAuthOptions,
+        user: { create: sinon.fake.throws(takenError)}
+      });
+      const status = 400;
+      const takenUserDetails = { email: 'taken@taken.com' };
+
+      const newUser = await signup(takenUserDetails);
+      expect(newUser).to.be.an('object').that.include({ status });
+    });
+
+    it('should return 500 when unexpected error had happened', async function () {
+      const takenError = new Error('Server failure.');
+      takenError.code =  'This is unexpected error code';
+      const { signup } = createAuthMethods({
+        ...commonAuthOptions,
+        user: { create: sinon.fake.throws(takenError)}
+      });
+      const status = 500;
+      const takenUserDetails = { email: 'taken@taken.com' };
+
+      const newUser = await signup(takenUserDetails);
+      expect(newUser).to.be.an('object').that.include({ status });
+    });
+
+    it('should await the user details before returning the user details', async function () {
+      const status = 201;
+      const { signup } = createAuthMethods({
+        ...commonAuthOptions,
+        user: {
+          create: sinon.fake.returns(new Promise((resolve, reject) => {
+            const clock = sinon.useFakeTimers();
+            clock.tick(0);
+            (clock) ? resolve (expectedUserDetails) : reject();
+          }))
+        }
+      });
+      const newUser = await signup(expectedUserDetails);
+      expect(newUser).to.be.an('object').that.include({ status });
+      expect(newUser.data).to.be.an('object').that.include({ ...expectedUserDetails, token });
+    });
   });
-  describe('Sign in', function () {
+  describe('Log in', function () {
     it('should return 400 when email or username is incorrect');
   });
 });
