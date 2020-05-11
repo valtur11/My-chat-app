386b9f6a729401f14fcc7d408a7a10a1f7c0531a
Valentin Ratchev <valentinratchev@gmail.com>
2020-05-11 17:08:11 +0300

    reduce test duplication

--- api/test/unit/authentication.spec.js
+++ api/test/unit/authentication.spec.js
@@ -42,64 +42,59 @@ describe('Authentication', function() {
   });
 
   describe('Sign up', function () {
-    it('should return status 201 with user details and token', async function () {
+    const takenUserDetails = { email: 'taken@taken.com' };
+    const takenError = new Error('Sorry, but this email or username is taken.');
+    takenError.code = 11000;
+    const serverError = new Error('Server failure.');
+    serverError.code = 'This is unexpected error code';
+    // singup service, independent from express
+    // res.status(obj.status).json(object.userDetails);
+    const tests = [
+      {
+        description: 'should return status 201 with user details and token',
+        userCreate: sinon.fake.returns(expectedUserDetails),
+        args: expectedUserDetails,
+        expected: [['data', {...expectedUserDetails, token}], ['', {status: 201}]]
+      },
+      {
+        description: 'should return 400 when email or username is taken',
+        userCreate: sinon.fake.throws(takenError),
+        args: takenUserDetails,
+        expected: [['', {status: 400}]]
+      },
+      {
+        description: 'should return 500 when unexpected error had happened',
+        userCreate: sinon.fake.throws(serverError),
+        args: expectedUserDetails,
+        expected: [['', {status: 500}]]
+      },
+      {
+        description: 'should await the user details before returning the user details',
+        userCreate: sinon.fake.returns(new Promise((resolve, reject) => {
+          const clock = sinon.useFakeTimers();
+          clock.tick(0);
+          (clock) ? resolve (expectedUserDetails) : reject();
+        })),
+        args: expectedUserDetails,
+        expected: [['data', {...expectedUserDetails, token}], ['', {status: 201}]]
+      }
+    ];
+
+    tests.forEach((test) => {
       const { signup } = createAuthMethods({
         ...commonAuthOptions,
-        user: { create: sinon.fake.returns(expectedUserDetails)}
+        user: { create: test.userCreate }
       });
-      // singup service, independent from express
-      // res.status(obj.status).json(object.userDetails);
-      const status = 201;
-      const newUser = await signup(expectedUserDetails);
-      expect(newUser).to.be.an('object').that.include({ status });
-      expect(newUser.data).to.be.an('object').that.include({ ...expectedUserDetails, token });
-    });
-
-    it('should return 400 when email or username is taken', async function () {
-      const takenError = new Error('Sorry, but this email or username is taken.');
-      takenError.code =  11000;
-      const { signup } = createAuthMethods({
-        ...commonAuthOptions,
-        user: { create: sinon.fake.throws(takenError)}
+      it(test.description, async () => {
+        const newUser = await signup(test.args);
+        test.expected.forEach((val) => {
+          const actual = newUser[val[0]] || newUser;
+          expect(actual).to.be.an('object').that.include(val[1]);
+        });
       });
-      const status = 400;
-      const takenUserDetails = { email: 'taken@taken.com' };
-
-      const newUser = await signup(takenUserDetails);
-      expect(newUser).to.be.an('object').that.include({ status });
-    });
-
-    it('should return 500 when unexpected error had happened', async function () {
-      const takenError = new Error('Server failure.');
-      takenError.code =  'This is unexpected error code';
-      const { signup } = createAuthMethods({
-        ...commonAuthOptions,
-        user: { create: sinon.fake.throws(takenError)}
-      });
-      const status = 500;
-      const takenUserDetails = { email: 'taken@taken.com' };
-
-      const newUser = await signup(takenUserDetails);
-      expect(newUser).to.be.an('object').that.include({ status });
-    });
-
-    it('should await the user details before returning the user details', async function () {
-      const status = 201;
-      const { signup } = createAuthMethods({
-        ...commonAuthOptions,
-        user: {
-          create: sinon.fake.returns(new Promise((resolve, reject) => {
-            const clock = sinon.useFakeTimers();
-            clock.tick(0);
-            (clock) ? resolve (expectedUserDetails) : reject();
-          }))
-        }
-      });
-      const newUser = await signup(expectedUserDetails);
-      expect(newUser).to.be.an('object').that.include({ status });
-      expect(newUser.data).to.be.an('object').that.include({ ...expectedUserDetails, token });
     });
   });
+
   describe('Log in', function () {
     it('should return 200 when email and password are correct', async () => {
       const expectedPassword = 'sxdcfvg';
