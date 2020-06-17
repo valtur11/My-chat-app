df4617a5c0c10631b8a04ec8b8753f0b3d2988c6
Valentin Ratchev <valentinratchev@gmail.com>
2020-06-17 23:38:58 +0300

    Add socket io auth

--- api/index.js
+++ api/index.js
@@ -4,12 +4,70 @@ const debug = require('debug')('My_chat_app: index.js *');
 const routes = require('./routes');
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server, { origins: '*:*'});
-const io_connection = require('./routes/io_connection');
+const io = require('socket.io')(server);
+io.set('origins', '*:*'); //@TODO only to server
+// const io_connection = require('./routes/io_connection');
+
+const auth = require('./authentication');
 
 app.use('/api', routes);
 
-io.on('connection', io_connection);
+let users = new Map();//nicknames
+let messages = [];
+
+function getKeyByValue(map, searchValue) {
+  for (let [key, value] of map.entries()) {
+    if (value === searchValue)
+      return key;
+  }
+}
+
+io.use((socket, next) => {
+  try {
+    let token = (socket.request.headers['authorization']) ? socket.request.headers['authorization'].split(' ')[1] : next();
+    const decoded = auth.verifyToken(token);
+    console.log(decoded.email, socket.id);
+    socket.username = decoded.email;
+    console.log(socket.username)
+    users.set(socket.username, socket.id);
+    //socket.friends
+    return next();
+  } catch (e) {
+    console.log('err')
+    return next();
+  }
+});
+io.on('connection', (socket) => {
+  if(getKeyByValue(users, socket.id) === undefined){
+    // socket.disconnect();
+  }
+  //a user connected, retrieve its messages from the db.
+  console.log('a user connected, sockets size:', users.size, socket.id);
+  socket.on('chat', (msg) => {
+    console.log('message: ' + msg);
+    messages.push({ author: socket.username, message: msg});
+    //if the socket is online, then emit, else save to the db.
+
+    socket.broadcast.emit('chat', msg);
+  });
+
+  socket.on('PM', (toUser, msg) => {
+    console.log(`PM to ${toUser} with message: ${msg} from ${getKeyByValue(users, socket.id)}`);
+    messages.push({ author: getKeyByValue(users, socket.id), message: msg});
+    //if the socket is online, then emit, else save to the db.
+    const socketId = users.get(toUser);
+    socketId ? io.to(socketId).emit('PM', msg) : socket.emit('offline');
+  });
+
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing');
+  });
+
+  socket.on('disconnect', () => {
+    users.delete(socket.username);
+    console.log('user disconnected, saving to the db');
+  });
+});
 
 server.listen(PORT, () => {
   debug(`Server with pid ${process.pid} started at PORT ${PORT}`);
--- api/routes/io_connection.js
+++ api/routes/io_connection.js
@@ -1,18 +1,2 @@
-let sockets = new Set();//nicknames
-let messages = [];
-module.exports = (socket) => {
-  sockets.add(socket);
-  console.log('a user connected, sockets size:', sockets.size);
-  socket.on('chat', (msg) => {
-    console.log('message: ' + msg);
-    messages.push({ author: socket, message: msg});
-    socket.broadcast.emit('chat', msg);
-  });
-  socket.on('typing', () => {
-    socket.broadcast.emit('typing');
-  });
-  socket.on('disconnect', () => {
-    sockets.delete(socket);
-    console.log('user disconnected, saving to the db');
-  });
-};
+
+module.exports = 3;
