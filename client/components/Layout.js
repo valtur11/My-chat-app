913313b8db7a7ce3047082f5f2775d7777f2fb78
Valentin Ratchev <valentinratchev@gmail.com>
2020-07-30 11:59:48 +0300

    Move header content and chat now button to navbar

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -1,27 +1,35 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { useRouter } from 'next/router';
 
 function Layout({ children, date }) {
+  const router = useRouter();
+
   return (
     <>
       <Head>
         <title>My chat app</title>
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'></meta>
-        <link rel='icon' href='/favicon.png' />
+        <link rel="icon" type="image/png" href="/favicon.png" />
       </Head>
 
-      <header className = 'sticky-top bg-white border-bottom shadow-sm'>
-        <nav className="navbar navbar-light bg-light">
-          <Link className="navbar-brand" href='/'>
-            <a> <img  src='/favicon.png' className='img-fluid' width='60' height='auto' /> <img  src='/profile.png' className='img-fluid' width='130' height='30' /> </a>
+      <header className = 'border-bottom shadow-sm bg-white'>
+        <nav className="navbar sticky-top navbar-light bg-light">
+          <Link href='/'>
+            <a className="navbar-brand"> <img src='/favicon.png' className='img-fluid' width='60' height='auto' /> <img  src='/profile.png' className='img-fluid' width='130' height='30' /> </a>
           </Link>
+          <ul className="d-md-flex d-block flex-row mx-md-auto mx-0 navbar-nav">
+            <li className={router.pathname === '/chat' ? 'nav-item active' : 'nav-item'}>
+              <Link href='/chat'>
+                <a className='nav-link'><button className='btn btn-primary btn-large'>Chat now</button></a>
+              </Link>
+
+            </li>
+          </ul>
         </nav>
       </header>
       <main role = 'main'>
-        <Link href='/chat'>
-          <a><button className='btn btn-primary btn-large'>Chat now</button></a>
-        </Link>
         {children}
       </main>
       <footer className = 'pt-4 my-md-5 pt-md-5 border-top text-muted'>
